db8173983cadfd2fcb7240ed4273abcaac1759d9
Patrick Scott <patrick.lee.scott@gmail.com>
2013-08-09 01:34:23 -0400

    user page and dynamic navbar

--- client/routers/router.js
+++ client/routers/router.js
@@ -1,32 +1,25 @@
-Meteor.Router.beforeRouting = function() {
-  console.log('// Before Routing //')
-  // reset all session variables that might be set by the previous route
-
-  $('body').css('min-height','0');
-
-}
-
 var user = function() {
   Session.set('currentUserId', Meteor.userId);
   return 'user';
 };
 
 var epk = function(name) {
-  console.log("Set to " + name + "'s EPK Mode");
-  Session.set('epkMode', true);
   Session.set('artistName', name);
   return 'epk'
 }
 
+var editEpk = function(name) {
+  Session.set('artistName', name);
+  Session.set('editMode', true);
+}
+
 Meteor.Router.add({
   '/': user,
-  '/:name': epk,
-  '/:name/edit': 'editEpk'
+  '/:name': {as: 'epk', to: epk},
+  '/:name/edit': {as: 'editEpk', to: editEpk}
 });
 
 Meteor.Router.filters({
-//
-
   'requireLogin': function(page) {
     if (Meteor.user())
       return page;
@@ -39,11 +32,16 @@ Meteor.Router.filters({
   'clearErrors': function(page) {
     clearErrors();
     return page;
+  },
+
+  setEpkMode: function(page) {
+    Session.set('epkMode', page === 'epk' ? true : false);
+    return page;
   }
 });
 
-
 Meteor.Router.filter('requireLogin', {except: ['404', 'epk']});
 Meteor.Router.filter('clearErrors');
-
+Meteor.Router.filter('setEpkMode');
 Meteor.Router.filter('requireUserIsCollaboratorForArtist', {only: 'editEpk'})
+
